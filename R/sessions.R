b635ca3a27acde959ceb9b79c2b242d213cdc579
EllaKaye <ella.kaye@gmail.com>
2024-09-28 20:58:32 +0100

    Away from template towards new site

--- R/sessions.R
+++ R/sessions.R
@@ -1,33 +1,22 @@
-create_session <- function(name) {
-  
-  # new slides
-  from_slides <- here::here("slides", "00-template")
-  to_slides <- here::here("slides", name)
+create_courses <- function(name) {
 
-  dir.create(to_slides)
-  file.copy(list.files(from_slides, full.names = TRUE),
-            to_slides,
+  # new courses
+  from_courses <- here::here("courses", "00-template")
+  to_courses <- here::here("courses", name)
+  dir.create(to_courses)
+  file.copy(list.files(from_courses, full.names = TRUE),
+            to_courses,
             recursive = TRUE)
   
-  # new content
-  from_content <- here::here("content", "00-template")
-  to_content <- here::here("content", name)
-  dir.create(to_content)
-  file.copy(list.files(from_content, full.names = TRUE),
-            to_content,
-            recursive = TRUE)
-  
-  index <- readLines(here::here("content", name, "index.qmd"))
+  index <- readLines(here::here("courses", name, "index.qmd"))
   index_new_links <- gsub("UPDATE-DIR-NAME", name, index)
-  writeLines(index_new_links, here::here("content", name, "index.qmd"))
+  writeLines(index_new_links, here::here("courses", name, "index.qmd"))
 
 }
 
 delete_session <- function(name) {
-  content <- here::here("content", name)
-  slides <- here::here("slides", name)
+  courses <- here::here("courses", name)
   
-  unlink(content, recursive = TRUE)
-  unlink(slides, recursive = TRUE)
+  unlink(courses, recursive = TRUE)
 }
 
